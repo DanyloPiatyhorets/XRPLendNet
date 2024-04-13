454c25bea9ff2b672c99baadf61077eb2746a35f
Dawe000 <dawid.pisarczyk@gmail.com>
2024-04-13 15:18:09 +0100

    Initial frontend commit

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,62 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { type Chain } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+const xrp = {
+  id: 1440002 ,
+  name: 'XRPL EVM Sidechain Devnet',
+  iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/52.png',
+  iconBackground: '#fff',
+  nativeCurrency: { name: 'XRP', symbol: 'XRP', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc-evm-sidechain.xrpl.org'] },
+  },
+  blockExplorers: {
+    default: { name: 'EVM sidechain explorer', url: 'https://evm-sidechain.xrpl.org' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 11_907_934,
+    },
+  },
+} as const satisfies Chain;
+
+
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    xrp,
+  ],
+  ssr: true,
+});
+
+const client = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;
